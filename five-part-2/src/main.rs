f253eeb1116a59707045e87de1dc3691b88e34d8
Ian Anderson <ian@iancanderson.com>
2019-12-09 07:01:26 -0500

    d5p2 jump instructions

---
+++ five-part-2/src/main.rs
@@ -0,0 +1,281 @@
+use std::io::{stdin,stdout,Write};
+use std::fmt;
+
+fn main() {
+    let program = vec![3,225,1,225,6,6,1100,1,238,225,104,0,1101,78,5,225,1,166,139,224,101,-74,224,224,4,224,1002,223,8,223,1001,224,6,224,1,223,224,223,1002,136,18,224,101,-918,224,224,4,224,1002,223,8,223,101,2,224,224,1,224,223,223,1001,83,84,224,1001,224,-139,224,4,224,102,8,223,223,101,3,224,224,1,224,223,223,1102,55,20,225,1101,53,94,225,2,217,87,224,1001,224,-2120,224,4,224,1002,223,8,223,1001,224,1,224,1,224,223,223,102,37,14,224,101,-185,224,224,4,224,1002,223,8,223,1001,224,1,224,1,224,223,223,1101,8,51,225,1102,46,15,225,1102,88,87,224,1001,224,-7656,224,4,224,102,8,223,223,101,7,224,224,1,223,224,223,1101,29,28,225,1101,58,43,224,1001,224,-101,224,4,224,1002,223,8,223,1001,224,6,224,1,224,223,223,1101,93,54,225,101,40,191,224,1001,224,-133,224,4,224,102,8,223,223,101,3,224,224,1,223,224,223,1101,40,79,225,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,1008,226,677,224,1002,223,2,223,1005,224,329,1001,223,1,223,1107,226,677,224,1002,223,2,223,1005,224,344,1001,223,1,223,8,677,226,224,1002,223,2,223,1006,224,359,1001,223,1,223,1108,226,677,224,1002,223,2,223,1006,224,374,101,1,223,223,1007,677,677,224,102,2,223,223,1006,224,389,1001,223,1,223,8,226,677,224,102,2,223,223,1006,224,404,101,1,223,223,1007,226,226,224,1002,223,2,223,1006,224,419,101,1,223,223,107,677,226,224,1002,223,2,223,1006,224,434,1001,223,1,223,1007,226,677,224,102,2,223,223,1005,224,449,101,1,223,223,1107,226,226,224,1002,223,2,223,1005,224,464,1001,223,1,223,107,226,226,224,102,2,223,223,1006,224,479,101,1,223,223,108,226,226,224,1002,223,2,223,1006,224,494,101,1,223,223,107,677,677,224,102,2,223,223,1005,224,509,1001,223,1,223,1008,677,677,224,1002,223,2,223,1006,224,524,101,1,223,223,1107,677,226,224,102,2,223,223,1006,224,539,1001,223,1,223,108,677,226,224,102,2,223,223,1006,224,554,1001,223,1,223,1108,677,226,224,102,2,223,223,1005,224,569,1001,223,1,223,8,677,677,224,1002,223,2,223,1005,224,584,1001,223,1,223,7,677,677,224,1002,223,2,223,1005,224,599,101,1,223,223,1108,226,226,224,102,2,223,223,1006,224,614,101,1,223,223,1008,226,226,224,1002,223,2,223,1005,224,629,101,1,223,223,7,677,226,224,102,2,223,223,1006,224,644,1001,223,1,223,7,226,677,224,102,2,223,223,1005,224,659,101,1,223,223,108,677,677,224,1002,223,2,223,1006,224,674,101,1,223,223,4,223,99,226];
+
+    run_intcode(program);
+}
+
+#[derive(Debug, PartialEq)]
+enum Opcode {
+    Add,
+    Multiply,
+    GetInput,
+    Print,
+    JumpIfTrue,
+    JumpIfFalse,
+    LessThan,
+    Equals,
+    EndOfProgram,
+}
+
+impl fmt::Display for Opcode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum ParameterMode {
+    Position,
+    Immediate,
+}
+
+fn char_to_parameter_mode(c: char) -> ParameterMode {
+    match c {
+        '0' => ParameterMode::Position,
+        '1' => ParameterMode::Immediate,
+        _ => panic!("invalid parameter mode")
+    }
+}
+
+// Opcode integer is now more complicated..
+fn int_to_opcode(int: i32) -> Opcode {
+    if int == 1 {
+        Opcode::Add
+    } else if int == 2 {
+        Opcode::Multiply
+    } else if int == 3 {
+        Opcode::GetInput
+    } else if int == 4 {
+        Opcode::Print
+    } else if int == 5 {
+        Opcode::JumpIfTrue
+    } else if int == 6 {
+        Opcode::JumpIfFalse
+    } else if int == 7 {
+        Opcode::LessThan
+    } else if int == 8 {
+        Opcode::Equals
+    } else {
+        Opcode::EndOfProgram
+    }
+}
+
+// Given the first value of an instruction, return
+// (Opcode, Vec<ParameterMode>)
+// The parameter mode vector represents the parameter modes
+// of each parameter in the instruction.
+fn parse_first_value(first_value: i32) -> (Opcode, Vec<ParameterMode>) {
+    // Get lst two digits for the opcode
+    // Convert to string, then take last two characters, then parse to int
+    // Needs to work for both "2" and "1002"
+    let mut as_string = first_value.to_string();
+
+    if as_string.len() <= 2 {
+        let opcode_string = as_string;
+        let opcode_int: i32 = opcode_string.parse().unwrap();
+        return (int_to_opcode(opcode_int), Vec::new());
+    } else {
+        let opcode_string = as_string.split_off(as_string.len() - 2);
+        let opcode_int: i32 = opcode_string.parse().unwrap();
+
+        let mut parameter_modes: Vec<ParameterMode> = Vec::new();
+
+        // Rest of string is parameter modes, from right to left
+        for parameter_mode_char in as_string.chars().rev() {
+            parameter_modes.push(char_to_parameter_mode(parameter_mode_char));
+        }
+
+        return (int_to_opcode(opcode_int), parameter_modes);
+    };
+}
+
+fn num_values_in_instruction(opcode: Opcode) -> usize {
+    match opcode {
+        Opcode::Add => 4,
+        Opcode::Multiply => 4,
+        Opcode::GetInput => 2,
+        Opcode::Print => 2,
+        Opcode::JumpIfTrue => 3,
+        Opcode::JumpIfFalse => 3,
+        Opcode::LessThan => 4,
+        Opcode::Equals => 4,
+        Opcode::EndOfProgram => 1,
+    }
+}
+
+fn get_input_as_int() -> i32 {
+    let mut s = String::new();
+    println!("Enter input:");
+    let _ = stdout().flush();
+    stdin().read_line(&mut s).expect("Did not enter a correct string");
+    if let Some('\n')=s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r')=s.chars().next_back() {
+        s.pop();
+    }
+    return s.parse().expect("Input was not a valid integer");
+}
+
+fn resolve_operands(program: &Vec<i32>, current_position: usize, parameter_modes: Vec<ParameterMode>, num_operands: usize) -> Vec<i32> {
+    // Loop over operands
+    let operands: Vec<i32> = (0..num_operands).map(|operand_offset| -> i32 {
+        let index = current_position + operand_offset + 1;
+        let operand = program[index];
+
+        let parameter_mode = parameter_modes.get(operand_offset).unwrap_or(&ParameterMode::Position);
+
+        return match parameter_mode {
+            ParameterMode::Position => program[operand as usize],
+            ParameterMode::Immediate => operand,
+        }
+    }).collect();
+
+    return operands;
+}
+
+fn run_intcode(mut program: Vec<i32>) -> Vec<i32> {
+    let mut current_position = 0;
+
+    loop {
+        let (current_opcode, parameter_modes) = parse_first_value(program[current_position]);
+
+        println!("Running {}", current_opcode);
+
+        if let Opcode::EndOfProgram = current_opcode {
+            break;
+        }
+
+        match current_opcode {
+            Opcode::Add => {
+                let result_location = program[current_position + 3] as usize;
+                let operands = resolve_operands(&program, current_position, parameter_modes, 2);
+                program[result_location] = operands[0] + operands[1];
+            }
+            Opcode::Multiply => {
+                let result_location = program[current_position + 3] as usize;
+                let operands = resolve_operands(&program, current_position, parameter_modes, 2);
+                program[result_location] = operands[0] * operands[1];
+            }
+            Opcode::GetInput => {
+                let input = get_input_as_int();
+                let operand = program[current_position + 1];
+                program[operand as usize] = input;
+            }
+            Opcode::Print => {
+                let operand = program[current_position + 1];
+                println!("Out: {}", program[operand as usize]);
+            }
+            Opcode::JumpIfTrue => {
+                let operands = resolve_operands(&program, current_position, parameter_modes, 1);
+
+                if operands[0] != 0 {
+                    current_position = program[current_position + 2] as usize;
+                    continue;
+                }
+            }
+            Opcode::JumpIfFalse => {
+                let operands = resolve_operands(&program, current_position, parameter_modes, 1);
+
+                if operands[0] == 0 {
+                    current_position = program[current_position + 2] as usize;
+                    continue;
+                }
+            }
+            _ => panic!("Unhandled Opcode: {}", current_opcode),
+        };
+
+        current_position += num_values_in_instruction(current_opcode);
+    }
+
+    return program;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_program_one() {
+        let program = vec![1,0,0,0,99];
+        let answer = run_intcode(program);
+        assert_eq!(answer, vec![2,0,0,0,99]);
+    }
+
+    #[test]
+    fn example_program_two() {
+        let program = vec![2,3,0,3,99];
+        let answer = run_intcode(program);
+        assert_eq!(answer, vec![2,3,0,6,99]);
+    }
+
+    #[test]
+    fn example_program_three() {
+        let program = vec![2,4,4,5,99,0];
+        let answer = run_intcode(program);
+        assert_eq!(answer, vec![2,4,4,5,99,9801]);
+    }
+
+    #[test]
+    fn example_program_four() {
+        let program = vec![1,1,1,4,99,5,6,0,99];
+        let answer = run_intcode(program);
+        assert_eq!(answer, vec![30,1,1,4,2,5,6,0,99]);
+    }
+
+    // #[test]
+    // fn opcode_3_and_4() {
+    //     let program = vec![1,1,1,4,99,5,6,0,99];
+    //     let answer = run_intcode(program);
+    //     assert_eq!(answer, vec![30,1,1,4,2,5,6,0,99]);
+    // }
+
+    #[test]
+    fn parameter_modes() {
+        let program = vec![1002,4,3,4,33];
+        let answer = run_intcode(program);
+        assert_eq!(answer, vec![1002,4,3,4,99]);
+    }
+
+    #[test]
+    fn test_parse_first_value() {
+        assert_eq!(
+            parse_first_value(1002),
+            (
+                Opcode::Multiply,
+                vec![ParameterMode::Position, ParameterMode::Immediate],
+            )
+        );
+    }
+
+    #[test]
+    fn test_jump_if_true_when_false() {
+        let program = vec![105, 0, 7, 1, 0, 0, 0, 99];
+        let answer = run_intcode(program);
+        assert_eq!(answer, vec![210, 0, 7, 1, 0, 0, 0, 99]);
+    }
+
+    #[test]
+    fn test_jump_if_true_when_true() {
+        let program = vec![105, 1, 7, 1, 0, 0, 0, 99];
+        let answer = run_intcode(program);
+        assert_eq!(answer, vec![105, 1, 7, 1, 0, 0, 0, 99]);
+    }
+
+    #[test]
+    fn test_jump_if_false_when_false() {
+        let program = vec![106, 0, 7, 1, 0, 0, 0, 99];
+        let answer = run_intcode(program);
+        assert_eq!(answer, vec![106, 0, 7, 1, 0, 0, 0, 99]);
+    }
+
+    #[test]
+    fn test_jump_if_false_when_true() {
+        let program = vec![106, 1, 7, 1, 0, 0, 0, 99];
+        let answer = run_intcode(program);
+        assert_eq!(answer, vec![212, 1, 7, 1, 0, 0, 0, 99]);
+    }
+}
