c5fce072ba1c054de9f5d55d41f88d093d44230a
Ian Anderson <ian@iancanderson.com>
2019-12-11 20:58:56 -0500

    d7p2 hook up outer loop

--- seven-part-2-threads/src/main.rs
+++ seven-part-2-threads/src/main.rs
@@ -1,5 +1,12 @@
 use std::thread;
 use std::sync::mpsc;
+use itertools::{Itertools, Permutations};
+
+fn main() {
+    let program = vec![3,8,1001,8,10,8,105,1,0,0,21,30,51,72,81,94,175,256,337,418,99999,3,9,101,5,9,9,4,9,99,3,9,1001,9,3,9,1002,9,2,9,1001,9,2,9,1002,9,5,9,4,9,99,3,9,1002,9,4,9,101,4,9,9,102,5,9,9,101,3,9,9,4,9,99,3,9,1002,9,4,9,4,9,99,3,9,102,3,9,9,1001,9,4,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,99,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,99];
+
+    println!("Max thruster signal: {}", max_thruster_signal_with_feedback(&program));
+}
 
 // Simple version of program
 //
@@ -16,14 +23,25 @@ use std::sync::mpsc;
 // Provide a starting value to the first amplifier
 // Read from the output of the last amplifier to get the result
 
-fn main() {
+fn max_thruster_signal_with_feedback(program: &Vec<i32>) -> i32 {
+    // Loop over all permutations of [0,1,2,3,4]
+    // Find permutation with max thruster signal
+
+    return vec![5,6,7,8,9].iter().permutations(5).map(|phase_setting_sequence| {
+        let signal = thruster_signal_with_feedback(&phase_setting_sequence, program);
+        println!("Sequence {:?} gives output signal: {}", phase_setting_sequence, signal);
+        return signal;
+    }).max().expect("There will definitely be a maximum");
+}
+
+fn thruster_signal_with_feedback(phase_setting_sequence: &Vec<&i32>, program: &Vec<i32>) -> i32 {
     // This channel goes into the first amplifier
     let (first_output, first_input) = mpsc::channel();
     let mut last_input: mpsc::Receiver<i32> = first_input;
 
     let mut child_threads = vec![];
 
-    for _ in vec![5,6,7,8,9] {
+    for _ in phase_setting_sequence {
         // This channel goes out of the amplifier
         let (tx, rx) = mpsc::channel();
 
@@ -54,6 +72,8 @@ fn main() {
 
     let received2 = last_input.recv().unwrap();
     println!("Received: {}", received2);
+
+    return received2;
 
     // How to know when all threads halt?
     // I guess the thread should return when they get a 99 instruction
