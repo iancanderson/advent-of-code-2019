2ff1d15b3e3013bf0f4fd2eae166cf8cc6c96e26
Ian Anderson <ian@iancanderson.com>
2019-12-11 20:52:46 -0500

    Make proof-of-concept threads feedback

--- seven-part-2-threads/src/main.rs
+++ seven-part-2-threads/src/main.rs
@@ -21,24 +21,42 @@ fn main() {
     let (first_output, first_input) = mpsc::channel();
     let mut last_input: mpsc::Receiver<i32> = first_input;
 
+    let mut child_threads = vec![];
+
     for _ in vec![5,6,7,8,9] {
         // This channel goes out of the amplifier
         let (tx, rx) = mpsc::channel();
 
-        thread::spawn(move || {
+        child_threads.push(thread::spawn(move || {
             let amp = Amplifier { output: tx, input: last_input };
 
             let received = amp.input.recv().unwrap();
             amp.output.send(received + 1).unwrap();
-        });
+
+            let received2 = amp.input.recv().unwrap();
+            amp.output.send(received2 + 1).unwrap();
+        }));
 
         last_input = rx;
     }
 
     // Send 0 into the first amplifier
     first_output.send(0).unwrap();
+
     let received = last_input.recv().unwrap();
     println!("Received: {}", received);
+
+    first_output.send(received).unwrap();
+
+    for child_thread in child_threads {
+        let _ = child_thread.join();
+    }
+
+    let received2 = last_input.recv().unwrap();
+    println!("Received: {}", received2);
+
+    // How to know when all threads halt?
+    // I guess the thread should return when they get a 99 instruction
 }
 
 struct Amplifier {
