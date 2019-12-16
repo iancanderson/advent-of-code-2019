c8fab105e0c07438e6a080960efbd51e84d7d0bf
Ian Anderson <ian@iancanderson.com>
2019-12-15 21:19:50 -0500

    d9 solution

--- nine/src/main.rs
+++ nine/src/main.rs
@@ -1,10 +1,17 @@
+use std::collections::HashMap;
 use std::io::{stdin,stdout,Write};
 use std::fmt;
 
 fn main() {
-    let program = vec![3,225,1,225,6,6,1100,1,238,225,104,0,1101,78,5,225,1,166,139,224,101,-74,224,224,4,224,1002,223,8,223,1001,224,6,224,1,223,224,223,1002,136,18,224,101,-918,224,224,4,224,1002,223,8,223,101,2,224,224,1,224,223,223,1001,83,84,224,1001,224,-139,224,4,224,102,8,223,223,101,3,224,224,1,224,223,223,1102,55,20,225,1101,53,94,225,2,217,87,224,1001,224,-2120,224,4,224,1002,223,8,223,1001,224,1,224,1,224,223,223,102,37,14,224,101,-185,224,224,4,224,1002,223,8,223,1001,224,1,224,1,224,223,223,1101,8,51,225,1102,46,15,225,1102,88,87,224,1001,224,-7656,224,4,224,102,8,223,223,101,7,224,224,1,223,224,223,1101,29,28,225,1101,58,43,224,1001,224,-101,224,4,224,1002,223,8,223,1001,224,6,224,1,224,223,223,1101,93,54,225,101,40,191,224,1001,224,-133,224,4,224,102,8,223,223,101,3,224,224,1,223,224,223,1101,40,79,225,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,1008,226,677,224,1002,223,2,223,1005,224,329,1001,223,1,223,1107,226,677,224,1002,223,2,223,1005,224,344,1001,223,1,223,8,677,226,224,1002,223,2,223,1006,224,359,1001,223,1,223,1108,226,677,224,1002,223,2,223,1006,224,374,101,1,223,223,1007,677,677,224,102,2,223,223,1006,224,389,1001,223,1,223,8,226,677,224,102,2,223,223,1006,224,404,101,1,223,223,1007,226,226,224,1002,223,2,223,1006,224,419,101,1,223,223,107,677,226,224,1002,223,2,223,1006,224,434,1001,223,1,223,1007,226,677,224,102,2,223,223,1005,224,449,101,1,223,223,1107,226,226,224,1002,223,2,223,1005,224,464,1001,223,1,223,107,226,226,224,102,2,223,223,1006,224,479,101,1,223,223,108,226,226,224,1002,223,2,223,1006,224,494,101,1,223,223,107,677,677,224,102,2,223,223,1005,224,509,1001,223,1,223,1008,677,677,224,1002,223,2,223,1006,224,524,101,1,223,223,1107,677,226,224,102,2,223,223,1006,224,539,1001,223,1,223,108,677,226,224,102,2,223,223,1006,224,554,1001,223,1,223,1108,677,226,224,102,2,223,223,1005,224,569,1001,223,1,223,8,677,677,224,1002,223,2,223,1005,224,584,1001,223,1,223,7,677,677,224,1002,223,2,223,1005,224,599,101,1,223,223,1108,226,226,224,102,2,223,223,1006,224,614,101,1,223,223,1008,226,226,224,1002,223,2,223,1005,224,629,101,1,223,223,7,677,226,224,102,2,223,223,1006,224,644,1001,223,1,223,7,226,677,224,102,2,223,223,1005,224,659,101,1,223,223,108,677,677,224,1002,223,2,223,1006,224,674,101,1,223,223,4,223,99,226];
+    let program = vec![1102,34463338,34463338,63,1007,63,34463338,63,1005,63,53,1102,1,3,1000,109,988,209,12,9,1000,209,6,209,3,203,0,1008,1000,1,63,1005,63,65,1008,1000,2,63,1005,63,902,1008,1000,0,63,1005,63,58,4,25,104,0,99,4,0,104,0,99,4,17,104,0,99,0,0,1102,1,37,1007,1102,24,1,1006,1102,26,1,1012,1101,528,0,1023,1102,256,1,1027,1102,466,1,1029,1102,1,629,1024,1101,0,620,1025,1101,0,0,1020,1102,1,30,1004,1101,39,0,1003,1102,36,1,1005,1102,531,1,1022,1102,32,1,1019,1101,0,27,1000,1101,0,28,1016,1101,1,0,1021,1101,23,0,1013,1102,1,25,1015,1102,1,21,1008,1102,1,22,1018,1102,1,34,1014,1102,475,1,1028,1101,33,0,1002,1101,0,35,1011,1102,1,20,1009,1102,38,1,1017,1101,259,0,1026,1101,31,0,1010,1101,0,29,1001,109,8,21102,40,1,10,1008,1018,40,63,1005,63,203,4,187,1105,1,207,1001,64,1,64,1002,64,2,64,109,7,21108,41,41,0,1005,1015,225,4,213,1106,0,229,1001,64,1,64,1002,64,2,64,109,1,1205,5,247,4,235,1001,64,1,64,1105,1,247,1002,64,2,64,109,20,2106,0,-9,1105,1,265,4,253,1001,64,1,64,1002,64,2,64,109,-38,1202,4,1,63,1008,63,33,63,1005,63,291,4,271,1001,64,1,64,1106,0,291,1002,64,2,64,109,6,2102,1,0,63,1008,63,29,63,1005,63,315,1001,64,1,64,1106,0,317,4,297,1002,64,2,64,109,10,21102,42,1,5,1008,1019,40,63,1005,63,341,1001,64,1,64,1105,1,343,4,323,1002,64,2,64,109,-13,2101,0,5,63,1008,63,24,63,1005,63,365,4,349,1105,1,369,1001,64,1,64,1002,64,2,64,109,7,1202,-6,1,63,1008,63,36,63,1005,63,389,1105,1,395,4,375,1001,64,1,64,1002,64,2,64,109,1,2107,31,-5,63,1005,63,411,1106,0,417,4,401,1001,64,1,64,1002,64,2,64,109,3,1206,8,431,4,423,1105,1,435,1001,64,1,64,1002,64,2,64,109,-8,2108,31,0,63,1005,63,451,1105,1,457,4,441,1001,64,1,64,1002,64,2,64,109,26,2106,0,-2,4,463,1001,64,1,64,1106,0,475,1002,64,2,64,109,-33,1207,6,38,63,1005,63,491,1106,0,497,4,481,1001,64,1,64,1002,64,2,64,109,3,2108,27,0,63,1005,63,515,4,503,1105,1,519,1001,64,1,64,1002,64,2,64,109,23,2105,1,0,1106,0,537,4,525,1001,64,1,64,1002,64,2,64,109,-30,1207,7,28,63,1005,63,559,4,543,1001,64,1,64,1106,0,559,1002,64,2,64,109,20,21101,43,0,0,1008,1013,43,63,1005,63,581,4,565,1105,1,585,1001,64,1,64,1002,64,2,64,109,-14,2102,1,1,63,1008,63,27,63,1005,63,611,4,591,1001,64,1,64,1105,1,611,1002,64,2,64,109,18,2105,1,7,4,617,1001,64,1,64,1106,0,629,1002,64,2,64,109,13,1206,-9,641,1105,1,647,4,635,1001,64,1,64,1002,64,2,64,109,-18,21107,44,45,-1,1005,1011,665,4,653,1105,1,669,1001,64,1,64,1002,64,2,64,109,-2,2107,28,-9,63,1005,63,687,4,675,1106,0,691,1001,64,1,64,1002,64,2,64,1205,10,701,1106,0,707,4,695,1001,64,1,64,1002,64,2,64,109,-6,1201,2,0,63,1008,63,21,63,1005,63,731,1001,64,1,64,1106,0,733,4,713,1002,64,2,64,109,-5,1208,7,23,63,1005,63,753,1001,64,1,64,1105,1,755,4,739,1002,64,2,64,109,16,1208,-8,37,63,1005,63,777,4,761,1001,64,1,64,1106,0,777,1002,64,2,64,109,3,21107,45,44,-8,1005,1010,797,1001,64,1,64,1105,1,799,4,783,1002,64,2,64,109,-8,1201,-5,0,63,1008,63,36,63,1005,63,821,4,805,1106,0,825,1001,64,1,64,1002,64,2,64,109,-9,2101,0,1,63,1008,63,31,63,1005,63,845,1105,1,851,4,831,1001,64,1,64,1002,64,2,64,109,6,21108,46,49,3,1005,1010,867,1106,0,873,4,857,1001,64,1,64,1002,64,2,64,109,5,21101,47,0,7,1008,1019,44,63,1005,63,897,1001,64,1,64,1106,0,899,4,879,4,64,99,21101,27,0,1,21102,913,1,0,1106,0,920,21201,1,30449,1,204,1,99,109,3,1207,-2,3,63,1005,63,962,21201,-2,-1,1,21101,940,0,0,1105,1,920,21202,1,1,-1,21201,-2,-3,1,21102,1,955,0,1106,0,920,22201,1,-1,-2,1105,1,966,22102,1,-2,-2,109,-3,2105,1,0];
 
-    run_intcode(program);
+    let (_, outputs) = run_intcode_with_output(program);
+
+    if outputs.len() == 1 {
+        println!("BOOST keycode: {}", outputs[0]);
+    } else {
+        println!("Something's not right. Outputs: {:?}", outputs);
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -17,6 +24,7 @@ enum Opcode {
     JumpIfFalse,
     LessThan,
     Equals,
+    AdjustRelativeBase,
     EndOfProgram,
 }
 
@@ -30,18 +38,20 @@ impl fmt::Display for Opcode {
 enum ParameterMode {
     Position,
     Immediate,
+    Relative,
 }
 
 fn char_to_parameter_mode(c: char) -> ParameterMode {
     match c {
         '0' => ParameterMode::Position,
         '1' => ParameterMode::Immediate,
+        '2' => ParameterMode::Relative,
         _ => panic!("invalid parameter mode: {}", c)
     }
 }
 
 // Opcode integer is now more complicated..
-fn int_to_opcode(int: i32) -> Opcode {
+fn int_to_opcode(int: i64) -> Opcode {
     if int == 1 {
         Opcode::Add
     } else if int == 2 {
@@ -58,8 +68,12 @@ fn int_to_opcode(int: i32) -> Opcode {
         Opcode::LessThan
     } else if int == 8 {
         Opcode::Equals
-    } else {
+    } else if int == 9 {
+        Opcode::AdjustRelativeBase
+    } else if int == 99 {
         Opcode::EndOfProgram
+    } else {
+        panic!("Unknown opcode")
     }
 }
 
@@ -67,7 +81,7 @@ fn int_to_opcode(int: i32) -> Opcode {
 // (Opcode, Vec<ParameterMode>)
 // The parameter mode vector represents the parameter modes
 // of each parameter in the instruction.
-fn parse_first_value(first_value: i32) -> (Opcode, Vec<ParameterMode>) {
+fn parse_first_value(first_value: i64) -> (Opcode, Vec<ParameterMode>) {
     // Get lst two digits for the opcode
     // Convert to string, then take last two characters, then parse to int
     // Needs to work for both "2" and "1002"
@@ -75,11 +89,11 @@ fn parse_first_value(first_value: i32) -> (Opcode, Vec<ParameterMode>) {
 
     if as_string.len() <= 2 {
         let opcode_string = as_string;
-        let opcode_int: i32 = opcode_string.parse().unwrap();
+        let opcode_int: i64 = opcode_string.parse().unwrap();
         return (int_to_opcode(opcode_int), Vec::new());
     } else {
         let opcode_string = as_string.split_off(as_string.len() - 2);
-        let opcode_int: i32 = opcode_string.parse().unwrap();
+        let opcode_int: i64 = opcode_string.parse().unwrap();
 
         let mut parameter_modes: Vec<ParameterMode> = Vec::new();
 
@@ -102,11 +116,12 @@ fn num_values_in_instruction(opcode: Opcode) -> usize {
         Opcode::JumpIfFalse => 3,
         Opcode::LessThan => 4,
         Opcode::Equals => 4,
+        Opcode::AdjustRelativeBase => 2,
         Opcode::EndOfProgram => 1,
     }
 }
 
-fn get_input_as_int() -> i32 {
+fn get_input_as_int() -> i64 {
     let mut s = String::new();
     println!("Enter input:");
     let _ = stdout().flush();
@@ -120,21 +135,42 @@ fn get_input_as_int() -> i32 {
     return s.parse().expect("Input was not a valid integer");
 }
 
-fn resolve_operands(program: &Vec<i32>, current_position: usize, parameter_modes: Vec<ParameterMode>, num_operands: usize) -> Vec<i32> {
+
+// Resolves to a location
+fn resolve_destination(ec: &ExecutionContext, parameter_modes: &Vec<ParameterMode>, position_offset: usize) -> i64 {
+    let parameter_mode = parameter_modes.get(position_offset - 1).unwrap_or(&ParameterMode::Position);
+
+    return match parameter_mode {
+        ParameterMode::Position => ec.program[ec.current_position + position_offset],
+        ParameterMode::Immediate => panic!("Immediate destination doesn't make sense"),
+        ParameterMode::Relative => {
+            let relative_index = ec.program[ec.current_position + position_offset];
+            return ec.relative_base + relative_index;
+        }
+    };
+}
+
+// Resolves to a value
+fn resolve_operands(ec: &ExecutionContext, parameter_modes: &Vec<ParameterMode>, num_operands: usize) -> Vec<i64> {
+    let debug = true;
+
     // Loop over operands
-    let operands: Vec<i32> = (0..num_operands).map(|operand_offset| -> i32 {
-        let index = current_position + operand_offset + 1;
-        let operand = program[index];
+    let operands: Vec<i64> = (0..num_operands).map(|operand_offset| -> i64 {
+        let index = ec.current_position + operand_offset + 1;
+        let operand = ec.program[index];
 
         let parameter_mode = parameter_modes.get(operand_offset).unwrap_or(&ParameterMode::Position);
 
         let resolved_operand = match parameter_mode {
-            ParameterMode::Position => program[operand as usize],
+            ParameterMode::Position => ec.read_value(operand),
             ParameterMode::Immediate => operand,
+            ParameterMode::Relative => ec.read_value(ec.relative_base as i64 + operand),
         };
 
-        // println!("raw operand: {}", operand);
-        // println!("resolved operand: {}", resolved_operand);
+        if debug {
+            println!("raw operand: {}", operand);
+            println!("resolved operand: {}", resolved_operand);
+        }
 
         return resolved_operand;
     }).collect();
@@ -142,86 +178,162 @@ fn resolve_operands(program: &Vec<i32>, current_position: usize, parameter_modes
     return operands;
 }
 
-fn run_intcode(mut program: Vec<i32>) -> Vec<i32> {
-    let mut current_position = 0;
+struct ExecutionContext {
+    program: Vec<i64>,
+    extended_memory: HashMap<i64, i64>,
+    relative_base: i64,
+    current_position: usize,
+}
+
+impl ExecutionContext {
+    // Either reads from a program register, or from extended memory
+    fn read_value(&self, index: i64) -> i64 {
+        if index < 0 { panic!("Can't access negative memory address") }
+
+        if self.program.len() > index as usize {
+            return self.program[index as usize];
+        } else {
+            // Memory defaults to 0
+            return *self.extended_memory.get(&index).unwrap_or(&0);
+        }
+    }
+
+    // Either sets a program register or extended memory register
+    fn set(&mut self, index: i64, value: i64) {
+        if index < 0 { panic!("Can't access negative memory address") }
+
+        if self.program.len() > index as usize {
+            self.program[index as usize] = value;
+        } else {
+            self.extended_memory.insert(index, value);
+        }
+    }
+
+    fn adjust_relative_base(&mut self, delta: i64) {
+        self.relative_base += delta;
+    }
+
+    fn first_value_of_current_line(&self) -> i64 {
+        return self.program[self.current_position];
+    }
+}
+
+fn run_intcode_with_output(program: Vec<i64>) -> (Vec<i64>, Vec<i64>) {
+    let mut outputs = Vec::new();
+    let debug = true;
+
+    let mut ec = ExecutionContext {
+        program,
+        extended_memory: HashMap::new(),
+        relative_base: 0,
+        current_position: 0,
+    };
 
     loop {
-        let (current_opcode, parameter_modes) = parse_first_value(program[current_position]);
+        let (current_opcode, parameter_modes) = parse_first_value(ec.first_value_of_current_line());
 
-        println!("Running {} at position {}", current_opcode, current_position);
+        if debug {
+            println!("");
+            println!("Running {} at position {}", current_opcode, ec.current_position);
+            println!("Relative base is {}", ec.relative_base);
+            println!("Parameter modes: {:?}", parameter_modes);
+        }
 
         match current_opcode {
             Opcode::Add => {
-                let result_location = program[current_position + 3] as usize;
-                let operands = resolve_operands(&program, current_position, parameter_modes, 2);
-                program[result_location] = operands[0] + operands[1];
+                let operands = resolve_operands(&ec, &parameter_modes, 2);
+                let destination = resolve_destination(&ec, &parameter_modes, 3);
+                ec.set(destination, operands[0] + operands[1]);
             }
             Opcode::Multiply => {
-                let result_location = program[current_position + 3] as usize;
-                let operands = resolve_operands(&program, current_position, parameter_modes, 2);
-                program[result_location] = operands[0] * operands[1];
+                let operands = resolve_operands(&ec, &parameter_modes, 2);
+                let destination = resolve_destination(&ec, &parameter_modes, 3);
+                ec.set(destination, operands[0] * operands[1]);
             }
             Opcode::GetInput => {
                 let input = get_input_as_int();
-                let operand = program[current_position + 1];
-                program[operand as usize] = input;
+                let destination = resolve_destination(&ec, &parameter_modes, 1);
+                ec.set(destination, input);
             }
             Opcode::Print => {
-                let operand = program[current_position + 1];
-                println!("Out: {}", program[operand as usize]);
+                let operands = resolve_operands(&ec, &parameter_modes, 1);
+                let output_value = operands[0];
+                println!("Out: {}", output_value);
+                outputs.push(output_value);
             }
             Opcode::JumpIfTrue => {
-                let operands = resolve_operands(&program, current_position, parameter_modes, 2);
+                let operands = resolve_operands(&ec, &parameter_modes, 2);
 
                 if operands[0] != 0 {
-                    current_position = operands[1] as usize;
+                    ec.current_position = operands[1] as usize;
                     continue;
                 }
             }
             Opcode::JumpIfFalse => {
-                let operands = resolve_operands(&program, current_position, parameter_modes, 2);
+                let operands = resolve_operands(&ec, &parameter_modes, 2);
 
                 if operands[0] == 0 {
-                    current_position = operands[1] as usize;
+                    ec.current_position = operands[1] as usize;
                     continue;
                 }
             }
             Opcode::LessThan => {
-                let operands = resolve_operands(&program, current_position, parameter_modes, 2);
-                let storage_location = program[current_position + 3] as usize;
+                let operands = resolve_operands(&ec, &parameter_modes, 2);
+                let destination = resolve_destination(&ec, &parameter_modes, 3);
 
-                program[storage_location] =
+                let value =
                     if operands[0] < operands[1] {
                         1
                     } else {
                         0
-                    }
+                    };
+
+                ec.set(destination, value);
             }
             Opcode::Equals => {
-                let operands = resolve_operands(&program, current_position, parameter_modes, 2);
-                let storage_location = program[current_position + 3] as usize;
-
-                program[storage_location] =
+                let operands = resolve_operands(&ec, &parameter_modes, 2);
+                let destination = resolve_destination(&ec, &parameter_modes, 3);
+                let value =
                     if operands[0] == operands[1] {
                         1
                     } else {
                         0
-                    }
+                    };
+
+                ec.set(destination, value);
+            }
+            Opcode::AdjustRelativeBase => {
+                let operands = resolve_operands(&ec, &parameter_modes, 1);
+                ec.adjust_relative_base(operands[0]);
             }
             Opcode::EndOfProgram => {
                 break;
             }
         };
 
-        current_position += num_values_in_instruction(current_opcode);
+        ec.current_position += num_values_in_instruction(current_opcode);
     }
 
-    return program;
+    return (ec.program, outputs);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn run_intcode(program: Vec<i64>) -> Vec<i64> {
+        return run_intcode_with_output(program).0;
+    }
+
+    #[test]
+    fn test_relative_destination() {
+        let program = vec![
+            109,5, // Set relative base to 5
+            20001,0,0,-5,99,
+        ];
+        let answer = run_intcode(program);
+        assert_eq!(answer, vec![218,5,20001,0,0,-5,99]);
+    }
 
     #[test]
     fn example_program_one() {
@@ -323,5 +435,26 @@ mod tests {
         let program = vec![108, 1, 2, 0, 99];
         let answer = run_intcode(program);
         assert_eq!(answer, vec![0, 1, 2, 0, 99]);
+    }
+
+    #[test]
+    fn test_nine_example_one() {
+        let program = vec![109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99];
+        let (_, outputs) = run_intcode_with_output(program.clone());
+        assert_eq!(outputs, program);
+    }
+
+    #[test]
+    fn test_nine_example_two() {
+        let program = vec![1102,34915192,34915192,7,4,7,99,0];
+        let (_, outputs) = run_intcode_with_output(program);
+        assert_eq!(outputs, [1219070632396864]);
+    }
+
+    #[test]
+    fn test_nine_example_three() {
+        let program = vec![104,1125899906842624,99];
+        let (_, outputs) = run_intcode_with_output(program);
+        assert_eq!(outputs, [1125899906842624]);
     }
 }
